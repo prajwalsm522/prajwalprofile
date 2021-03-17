149766ed898a6ce98f09ecbc8bb0e11aba6c939f
prajwalsm522 <smprajwal0@gmail.com>
2021-03-17 21:30:50 +0530

    first commit
    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+my_Favorite_number = 1
+if my_Favorite_number==10:
+    print("yea its working")
+elif my_Favorite_number==16:
+    print("yea elif its working")
+else:
+    print("no number matched")
